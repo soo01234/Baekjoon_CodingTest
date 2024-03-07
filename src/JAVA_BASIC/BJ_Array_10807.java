49f20a90b84155a8316e810f1c5805cd90f6ebc0
Soohyun <kwonsoohyun97@gmail.com>
2024-03-07 20:29:37 +0900

    0307_1

--- src/JAVA_BASIC/BJ_Array_10807.java
+++ src/JAVA_BASIC/BJ_Array_10807.java
@@ -1,4 +1,24 @@
 package JAVA_BASIC;
+import java.util.Scanner;
+public class BJ_Array_10807 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);  // Scanner 객체 생성
 
-public class BJ_10807 {
+        int count = 0;
+        int n = Integer.parseInt(in.nextLine());
+        int[] arr = new int[n];  // 방 n개 !!
+
+        String[] nums = in.nextLine().split(" ");
+
+        int v = in.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(nums[i]);
+            if (arr[i] == v) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
 }
